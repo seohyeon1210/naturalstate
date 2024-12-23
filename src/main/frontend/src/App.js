88262107935ad8733220b9a1cfccd1ce3fcf4014
seohyeon1210 <seohyeon174@naver.com>
2024-12-23 22:30:06 +0900

    Header

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,20 +1,28 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import Header from './component/header/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [mainMessage, setmainMessage] = useState('')
+    const [mainMessage, setmainMessage] = useState('')
 
-  useEffect(() => {
-    axios.get('/main')
-        .then(response => setmainMessage(response.data))
-        .catch(error => console.log(error))
-  }, []);
+    useEffect(() => {
+        axios.get('/main')
+            .then(response => setmainMessage(response.data))
+            .catch(error => console.log(error))
+    }, []);
 
-  return (
-      <div>
-        백엔드에서 가져온 데이터 확인 : {mainMessage}
-      </div>
-  );
+    return (
+        <>
+            <div>
+                <Header/>
+            </div>
+
+            <div>
+                가장 먼저 보여지는 백엔드에서 가져온 데이터 확인 : {mainMessage}
+            </div>
+        </>
+    );
 }
 
 export default App;
