489bb0e7a4366a8409122d739cdd4d8b5c0bd730
gunwoo <apmidbug@naver.com>
2024-12-29 14:08:07 +0900

    건우 12/29

--- src/main/frontend/src/components/customerService/App.js
+++ src/main/frontend/src/components/customerService/App.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import './App.css';  // CSS 스타일 파일 가져오기
-
-// MyButton 컴포넌트를 사용하려면 별도의 파일로 분리하거나 App.js 안에 포함시킬 수 있습니다.
+import React, { useEffect, useState } from 'react';
 import MyButton from './MyButton';  // MyButton 컴포넌트 가져오기
+import './customerService.css'; // CSS 파일 가져오기
 
 function App() {
   return (
     <div className="App">
       <h1>안녕안녕 여긴 고객센터 페이지야</h1>
       <h2>여기선 문의를 할수있어</h2>
+      <div id="root"></div>
       <MyButton />
     </div>
   );
--- src/main/frontend/src/components/customerService/customerService.js
+++ src/main/frontend/src/components/customerService/customerService.js
@@ -3,7 +3,8 @@ import React from 'react'; // 반드시 추가해야 함
 function App() {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <h1>안녕안녕 여긴 고객센터 페이지야</h1>
+       <h2>여기선 문의를 할수있어</h2>
     </div>
   );
 }
