f9e9bc3e6a10f3965df0a50800aa1708cc47ce38
seohyeon1210 <seohyeon174@naver.com>
2025-01-07 11:21:50 +0900

    main

--- src/main/frontend/src/components/Login/Login.js
+++ src/main/frontend/src/components/Login/Login.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import { useNavigate } from "react-router-dom";
+import "./Login.css";
 
 function Login({ onLogin }) {
     const [userId, setUserId] = useState("");
@@ -37,6 +40,26 @@ function Login({ onLogin }) {
 
     return (
         <div>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>아이디</Form.Label>
+                    <Form.Control type="email" placeholder="아이디" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>비밀번호</Form.Label>
+                    <Form.Control type="password" placeholder="비밀번호" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="아이디 기억하기" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    로그인
+                </Button>
+            </Form>
+
+            {/*기존 로그인*/}
+
             <h2>로그인</h2>
             <form onSubmit={handleSubmit}>
                 <div>
