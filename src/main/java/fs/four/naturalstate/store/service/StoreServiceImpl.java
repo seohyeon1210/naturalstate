22ca965f2551247a9e0ff327cd836d20e47fb236
seohyeon1210 <seohyeon174@naver.com>
2025-01-14 10:31:35 +0900

    storejoin

--- src/main/java/fs/four/naturalstate/store/service/StoreServiceImpl.java
+++ src/main/java/fs/four/naturalstate/store/service/StoreServiceImpl.java
@@ -3,6 +3,7 @@ package fs.four.naturalstate.store.service;
 import fs.four.naturalstate.store.dao.StoreDAO;
 import fs.four.naturalstate.store.vo.StoreVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,12 +11,15 @@ import org.springframework.transaction.annotation.Transactional;
 @Service("storeService")
 @Transactional(propagation = Propagation.REQUIRED)
 public class StoreServiceImpl implements StoreService {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     StoreDAO storeDAO;
 
     @Override
     public int addStore(StoreVO storeVO) throws Exception{
+        storeVO.setPassword(passwordEncoder.encode(storeVO.getPassword()));
         return storeDAO.insertStore(storeVO);
     }
 }
